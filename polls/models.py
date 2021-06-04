cb5bba02b4e87ee5ad818f95482fec3bea6b7604
Alex <sobachka.94@mail.ru>
2021-06-04 07:41:19 +0300

    create models and migrate

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,46 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    #choices
+    _TEXT = 'text'
+    _ONE = 'choose_one'
+    _MANY = 'choose_many'
+    TYPE_CHOICES = [(_TEXT, 'text'),
+                    (_ONE, 'choose one answer'),
+                    (_MANY, 'choose one or more answers')]
+    text = models.CharField(default='', null=True, max_length=240)
+    question_type = models.CharField(choices=TYPE_CHOICES, blank=True, default=_TEXT, max_length=20)
+
+    def __str__(self):
+        return f'{self.question_type}: {self.text}'
+
+
+class Poll(models.Model):
+    name = models.CharField(default='NoName', null=True, max_length=80)
+    date_start = models.DateTimeField(default=datetime.now, blank=True, editable=False)
+    date_end = models.DateTimeField(blank=True)
+    description = models.CharField(default='', null=True, max_length=80)
+    questions = models.ManyToManyField(Question, blank=True)
+
+    def __str__(self):
+        return f'{self.name}: {self.date_start} -- {self.date_end}'
+
+
+class FinishedPoll(models.Model):
+    poll = models.ForeignKey(Poll, blank=True, on_delete=models.CASCADE)
+    # we don't need full user functionality
+    user_id = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return f'user_id: {self.user_id}, poll_id: {self.poll.id}'
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, blank=True, on_delete=models.CASCADE)
+    finished_poll = models.ForeignKey(FinishedPoll, related_name='answers', blank=True, on_delete=models.CASCADE)
+    text = models.CharField(default='', null=True, max_length=360)
+
+    def __str__(self):
+        return f'question_id: {self.question.id}, finished_poll_id: {self.finished_poll.id}, text: {self.text}'
