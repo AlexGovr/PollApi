7c7580d08fbcf273b9b993368cb3a02a6034ceca
Alex <sobachka.94@mail.ru>
2021-06-05 09:32:24 +0300

    add serializers

---
+++ polls/serializers.py
@@ -0,0 +1,39 @@
+
+from .models import Poll, Question, FinishedPoll, Answer, QuestionChoice
+from rest_framework import serializers
+
+
+class PollSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Poll
+        fields = ['name', 'date_start', 'date_end', 'description', 'questions', 'id']
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    choices = serializers.PrimaryKeyRelatedField(many=True, required=False, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = ['text', 'question_type', 'id', 'choices']
+
+
+class FinishedPollSerializer(serializers.ModelSerializer):
+    answers = serializers.StringRelatedField(many=True)
+    class Meta:
+        model = FinishedPoll
+        fields = ['user_id', 'poll', 'id', 'answers']
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Answer
+        fields = ['text', 'question', 'id', 'finished_poll']
+
+
+class QuestionChoiceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = QuestionChoice
+        fields = ['text', 'question', 'id']
