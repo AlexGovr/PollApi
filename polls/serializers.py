6f0e874d302858c2ef7c27feb468e1f4c8bcb208
Alex <sobachka.94@mail.ru>
2021-06-14 19:52:58 +0300

    implement nested serialization

--- polls/serializers.py
+++ polls/serializers.py
@@ -1,25 +1,51 @@
 
-from .models import Poll, Question, FinishedPoll, Answer
+from .models import Poll, Question, FinishedPoll, Answer, Choice
 from rest_framework import serializers
 
 
-class PollSerializer(serializers.ModelSerializer):
-
+class ChoiceSerializer(serializers.ModelSerializer):
+    question = serializers.PrimaryKeyRelatedField(required=False, queryset=Question.objects.all())
     class Meta:
-        model = Poll
-        fields = ['name', 'date_start', 'date_end', 'description', 'id']
+        model = Choice
+        fields = ['text', 'question', ]
 
 
 class QuestionSerializer(serializers.ModelSerializer):
-
+    choices = ChoiceSerializer(many=True, required=False)
+    poll = serializers.PrimaryKeyRelatedField(required=False, queryset=Poll.objects.all())
     class Meta:
         model = Question
-        fields = ['text', 'question_type', 'id', 'poll',]
+        fields = ['text', 'question_type', 'id', 'poll', 'choices', ]
+    
+    def create(self, validated_data):
+        try:
+            choices_data = validated_data.pop('choices')
+            question = super().create(validated_data)
+            if validated_data.get('question_type', '') in (Question._ONE, Question._MANY):
+                srl = ChoiceSerializer()
+                for ch in choices_data:
+                    srl.create({'question': question, **ch})
+        except KeyError:
+            question = super().create(validated_data)
+        return question
+
+
+class PollSerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True)
+    class Meta:
+        model = Poll
+        fields = ['name', 'date_start', 'date_end', 'description', 'id', 'questions']
+    
+    def create(self, validated_data):
+        questions_data = validated_data.pop('questions')
+        poll = super().create(validated_data)
+        srl = QuestionSerializer()
+        for q in questions_data:
+            srl.create({'poll': poll, **q})
+        return poll
 
 
 class FinishedPollSerializer(serializers.ModelSerializer):
-    answers = serializers.PrimaryKeyRelatedField(many=True, required=True, queryset=Answer.objects.all())
-
     class Meta:
         model = FinishedPoll
         fields = ['user_id', 'poll', 'id', 'answers']
